6a5e7a04ca9f7e426e5a72630b2b6984f4976c0c
Whilmar Bitoco <117832470+whilmarbitoco@users.noreply.github.com>
2023-03-10 15:09:04 +0000

    Added openAI to the app.

--- src/App.vue
+++ src/App.vue
@@ -12,7 +12,11 @@ import Navbar from './components/Navbar.vue';
 import Chats from './components/Chats.vue';
 import Form from './components/Form.vue';
 import Footer from './components/Footer.vue';
+import { Configuration, OpenAIApi } from "openai";
 
+
+
+  
 export default {
     name: "App",
     components: {
@@ -24,22 +28,53 @@ export default {
     data() {
         return {
             chats: [],
-            receive: [{id:1, msg: "hello"}],
+            receive: [],
             state: false,
         };
     },
     methods: {
-        sendMessage(chats) {
-            
-            if (chats) {
-                const newChat = {
-                    id: Math.floor((Math.random() * 19244727) / 10),
-                    msg: chats.msg,
-                };
-                this.chats.push(newChat);
-                this.state = true;
-            }
-        },
+
+      async sendMessage(chats) {
+  if (chats) {
+    const newChat = {
+      id: Math.floor((Math.random() * 19244727) / 10),
+      msg: chats.msg,
+    };
+    this.chats.push(newChat);
+    this.state = true;
+
+    try {
+      const apikey = "<your openai api key>";
+
+      const configuration = new Configuration({
+  apiKey: apikey
+});
+const openai = new OpenAIApi(configuration);
+      const com = await openai.createCompletion({
+    model: "text-davinci-003",
+    prompt: chats.msg,
+    max_tokens: 2048, 
+    temperature: 0,
+    top_p:1.0,
+    frequency_penalty: 0.0,
+    presence_penalty: 0.0
+    
+  });
+
+    const newRes = {
+      id: Math.floor((Math.random() * 19244727) / 10),
+      msg: com.data.choices[0].text,
+    };
+    this.receive = [newRes, ...this.receive];
+      
+      
+    } catch (err) {
+      console.log(err);
+    }
+  }
+}
+
+
     },
 };
 </script>
